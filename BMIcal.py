b207ae84fd9281a743d2901cf1486893e4135d2e
Devansh Joshi <111854595+theBotNinja@users.noreply.github.com>
2023-06-29 11:30:57 +0530

    Create BMIcal.py

---
+++ BMIcal.py
@@ -0,0 +1,15 @@
+height = float(input("Enter the height in cm: "))  
+weight = float(input("Enter the weight in kg: "))  
+  
+BMI = weight / (height/100)**2  
+  
+print("Your Body Mass Index is", BMI)  
+ 
+if BMI <= 18.5:  
+    print("You are underweight.")  
+elif BMI <= 24.9:  
+    print("You are healthy.")  
+elif BMI <= 29.9:  
+    print("You are over weight.")  
+else:  
+    print("You are obese.") 
