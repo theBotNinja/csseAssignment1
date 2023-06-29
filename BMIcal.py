7cab9d9b0c3a1072f36f058eb170797f20813f05
Devansh Joshi <111854595+theBotNinja@users.noreply.github.com>
2023-06-29 12:21:05 +0530

    Update BMIcal.py

--- BMIcal.py
+++ BMIcal.py
@@ -2,7 +2,7 @@ height = float(input("Enter the height in cm: "))
 weight = float(input("Enter the weight in kg: "))  
   
 BMI = weight / (height/100)**2  
-  
+
 print("Your Body Mass Index is", BMI)  
  
 if BMI <= 18.5:  
